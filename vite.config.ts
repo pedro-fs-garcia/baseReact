7ec3a2e50fc03c3955e4527dcf6dbd411fe1e4ea
Pedro Garcia <pedrofsgarcia.pro@gmail.com>
2025-06-18 22:31:18 -0300

    tailwind local

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import path from 'path'
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
+import tailwindcss from '@tailwindcss/vite'
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
